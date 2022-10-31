e5c346d4c1f5d094c6cf4c69a3c749b18afed490
moh1idris <113855948+moh1idris@users.noreply.github.com>
2022-10-31 23:48:29 +0530

    useState Hook

--- chapter-01/src/Content.js
+++ chapter-01/src/Content.js
@@ -1,20 +1,36 @@
 
 import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
+   const [name, setName] = useState('Ajju');
+   const [count, setCount] = useState(0);
     const handleNameChange = () => {
-        const names = ['bob' , 'job' , 'khob']
+        const names = ['Ajju' , 'job' , 'khob']
         const int = Math.floor(Math.random() * 3)    //--> this will take random no from 0-3
-        return names[int];
+        setName(names[int])                          //--> as we are using useSate so we can edit this one itself
      }     
      
-     const handleClick =() =>{
-      console.log('you clicked it ');
+   const handleClick =() =>{
+      setCount(count + 1);
+      console.log(count);
      }
 
-     const handleClick2 =(name) =>{
-      console.log(`${name} was Clicked `);
-     }
+    
+
+   // const handleNameChange = () => {
+   //    const names = ['Ajju' , 'job' , 'khob']
+   //    const int = Math.floor(Math.random() * 3)    //--> this will take random no from 0-3
+   //    return names[int];
+   // } 
+
+   //   const handleClick =() =>{
+   //    console.log('you clicked it ');
+   //   }
+
+   //   const handleClick2 =(name) =>{
+   //    console.log(`${name} was Clicked `);
+   //   }
 
      const handleClick3 =(e) =>{                      //--> passing the event 
       // console.log(e)                               //--> this will give the event object 
@@ -25,13 +41,18 @@ const Content = () => {
   return (
     <main>
        <p onDoubleClick={handleClick}>          {/* this will shoe in console a su will click o the name */}
-        Hello {handleNameChange()}!
+        {/* Hello {handleNameChange()}! */}
+        Hello {name}!
        </p>
-       <button onClick={handleClick}>Click it</button>
+       {/* <button onClick={handleClick}>Click it</button> */}
        {/* when we do not passs parameter then use this  */}
-       <button onClick={() => handleClick2('AJJU')}>Click it</button>
-       <button onClick={(e) => handleClick3(e)}>Click it</button>   {/* this will give the object in which u will be having all the evenets  */}
-      
+       {/* <button onClick={() => handleClick2('AJJU')}>Click it</button>
+       <button onClick={(e) => handleClick3(e)}>Click it</button>   this will give the object in which u will be having all the evenets  */}
+
+
+        {/* useSate */}
+       <button onClick={handleNameChange}>Change name</button>
+       <button onClick={handleClick}>Click it</button>
     </main>
   )
 }
