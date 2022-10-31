704c5549df6dac75dc69bdc77ba55b6bd8c85ac6
moh1idris <113855948+moh1idris@users.noreply.github.com>
2022-10-31 23:31:32 +0530

    Click Events

--- chapter-01/src/Content.js
+++ chapter-01/src/Content.js
@@ -8,11 +8,30 @@ const Content = () => {
         return names[int];
      }     
      
+     const handleClick =() =>{
+      console.log('you clicked it ');
+     }
+
+     const handleClick2 =(name) =>{
+      console.log(`${name} was Clicked `);
+     }
+
+     const handleClick3 =(e) =>{                      //--> passing the event 
+      // console.log(e)                               //--> this will give the event object 
+      // console.log(e.target)                        //--><button>click it </button>
+      console.log(e.target.innerText)                 //--> click it 
+     }
+
   return (
     <main>
-       <p>
+       <p onDoubleClick={handleClick}>          {/* this will shoe in console a su will click o the name */}
         Hello {handleNameChange()}!
        </p>
+       <button onClick={handleClick}>Click it</button>
+       {/* when we do not passs parameter then use this  */}
+       <button onClick={() => handleClick2('AJJU')}>Click it</button>
+       <button onClick={(e) => handleClick3(e)}>Click it</button>   {/* this will give the object in which u will be having all the evenets  */}
+      
     </main>
   )
 }
